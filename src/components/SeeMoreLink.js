0ed8380988b9727a991f7e05d9221f8ca0339efc
Rafael Hernandez <rafaelhernandez@nypl.org>
2016-03-01 15:57:51 -0500

    Updated naming, removed onClick handler for Link component, cleaned up tab formatting and added defaultClass camelCase

--- src/components/SeeMoreLink.js
+++ src/components/SeeMoreLink.js
@@ -2,12 +2,6 @@ import React from 'react';
 import radium from 'radium';
 
 const styles = {
-  svg: {
-//    width: '10px',
-//    position: 'relative',
-//    left: '5.5px',
-//    top: '6px',
-  },
   circle: {
     strokeWidth: '3px',
     fill: 'none',
@@ -15,39 +9,28 @@ const styles = {
 };
 
 class SeeMoreLink extends React.Component {
-  // Constructor used in ES6
   constructor(props) {
     super(props);
-
-    this._onClick = this._onClick.bind(this);
-  }
-
-  _onClick(e) {
-    e.preventDefault();
-    this.props.onClick();
   }
 
   render() {
     return (
       <a
         id={this.props.id}
-        className={this.props.className + ' btn__icon'}
+        className={`${this.props.defaultClass} ${this.props.className}`}
         href={this.props.target}
         style={[styles.link]}
       >
-
-    <svg viewBox="0 0 50 50" height="25" width="25">
-        <circle cx="25" cy="25" r="23.4" style={[styles.circle]} />
-      <circle cx="15.5" cy="25" r="2.5" />
-      <circle cx="25" cy="25" r="2.5" />
-      <circle cx="34.5" cy="25" r="2.5" />
-    </svg>
-
-        <span className="iconLink-sm-label visuallyHidden">
+        <svg viewBox="0 0 50 50" height="25" width="25">
+          <circle cx="25" cy="25" r="23.4" style={[styles.circle]} />
+          <circle cx="15.5" cy="25" r="2.5" />
+          <circle cx="25" cy="25" r="2.5" />
+          <circle cx="34.5" cy="25" r="2.5" />
+        </svg>
+        <span className={`${this.props.defaultClass}-label visuallyHidden`}>
           {this.props.label}
         </span>
       </a>
-
     );
   }
 }
@@ -55,20 +38,20 @@ class SeeMoreLink extends React.Component {
 SeeMoreLink.propTypes = {
   id: React.PropTypes.string.isRequired,
   className: React.PropTypes.string.isRequired,
+  defaultClass: React.PropTypes.string,
   label: React.PropTypes.string,
   lang: React.PropTypes.string,
   target: React.PropTypes.string,
   style: React.PropTypes.object,
-  onClick: React.PropTypes.func,
 };
 
 SeeMoreLink.defaultProps = {
-  id: 'SeeMoreButton',
-  className: 'seeMoreButton',
+  id: 'SeeMoreLink',
+  className: '',
+  defaultClass: 'seeMoreLink',
   label: 'See More',
   lang: 'en',
   target: '#',
-  onClick() {},
 };
 
 export default radium(SeeMoreLink);
