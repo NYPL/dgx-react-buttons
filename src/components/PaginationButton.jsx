15c72e691f90f63c8bd055607822b44867533dd0
Kang Peng <kangtingpeng@nypl.org>
2016-08-24 11:55:57 -0400

    Added default styles for Pagination button.

--- src/components/PaginationButton.jsx
+++ src/components/PaginationButton.jsx
@@ -1,19 +1,7 @@
 // Import React Libraries
 import React from 'react';
-// import radium from 'radium';
 
 const PaginationButton = (props) => {
-  const styles = {
-    base: {
-      color: '#A3A19E',
-      border: '2px solid #A3A19E',
-    },
-    dots: {
-      border: '3px solid #A3A19E',
-    },
-  };
-
-  // render() {
   const dotElements = [];
   // Add loading class and the loading animation if it is loading now
   const isLoading = props.isLoading ? 'loading' : '';
@@ -27,7 +15,7 @@ const PaginationButton = (props) => {
         id={`${props.id}__dot-row__element_${i}`}
         className={`${props.className}__dot-row__element ${isLoading}`}
         key={i}
-        style={styles.dots}
+        style={props.dotStyle}
       >
       </span>
     );
@@ -38,7 +26,7 @@ const PaginationButton = (props) => {
       id={props.id}
       className={`${props.className}`}
       onClick={props.onClick}
-      style={styles.base}
+      style={props.style}
     >
       <span
         id={`${props.id}__dot-row`}
@@ -54,59 +42,7 @@ const PaginationButton = (props) => {
       </span>
     </button>
   );
-  // }
 };
-
-// class PaginationButton extends React.Component {
-//   render() {
-//     const dotElements = [];
-//     // Add loading class and the loading animation if it is loading now
-//     const isLoading = this.props.isLoading ? 'loading' : '';
-//     let i;
-
-//     // Generate the dots for the pagination button.
-//     // The number of the dots is determinated by the props.
-//     for (i = 0; i < this.props.dots; i++) {
-//       dotElements.push(
-//         <span
-//           id={`${this.props.id}__dot-row__element_${i}`}
-//           className={`${this.props.className}__dot-row__element ${isLoading}`}
-//           key={i}
-//           style={[
-//             styles.dots,
-//             this.props.dotStyle,
-//           ]}
-//         >
-//         </span>
-//       );
-//     }
-
-//     return (
-//       <button
-//         id={this.props.id}
-//         className={`${this.props.className}`}
-//         onClick={this.props.onClick}
-//         style={[
-//           styles.base,
-//           this.props.style,
-//         ]}
-//       >
-//         <span
-//           id={`${this.props.id}__dot-row`}
-//           className={`${this.props.className}__dot-row`}
-//         >
-//           {dotElements}
-//           <label
-//             id={`${this.props.id}__dot-row__number`}
-//             className={`${this.props.className}__dot-row__number`}
-//           >
-//             {this.props.label}
-//           </label>
-//         </span>
-//       </button>
-//     );
-//   }
-// }
 
 PaginationButton.propTypes = {
   id: React.PropTypes.string.isRequired,
@@ -128,6 +64,13 @@ PaginationButton.defaultProps = {
   label: 'Pagination Button',
   lang: 'en',
   dots: 3,
+  style: {
+    color: '#A3A19E',
+    border: '2px solid #A3A19E',
+  },
+  dotStyle: {
+    border: '3px solid #A3A19E',
+  },
 };
 
 // Export the component
