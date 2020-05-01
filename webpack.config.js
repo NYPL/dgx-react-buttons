06fc8efd3a00b4f700f0e6d974f81b23c6246565
Gregory Kallenberg <gregorykallenberg@nypl.org>
2020-05-01 09:24:25 -0400

    add globalObject to webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ if (process.env.NODE_ENV !== 'development') {
       path: path.join(rootPath, '/dist'),
       filename: 'index.min.js',
       libraryTarget: "umd",
-      library: "dgxReactButtons"
+      library: "dgxReactButtons",
+      globalObject: "this"
     },
     externals: {
       // Required in order to ignore library within other components
